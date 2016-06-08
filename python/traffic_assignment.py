f59f308994b7ad6288bd9110610258702e853490
Michael Souza <souza.michael@gmail.com>
2016-06-08 20:39:40 -0300

    Add ITA

--- python/traffic_assignment.py
+++ python/traffic_assignment.py
@@ -1,34 +1,96 @@
 import numpy as np
 import heapq
 import pandas as pd
+from scipy.sparse import csr_matrix
 
-def dijkstra(G, i, etol=0):
-    n = len(G.keys())              # number of nodes
-    d = [np.inf for k in range(n)] # d[i] = distance to node i
-    p = np.zeros(n, dtype=np.int)  # p[i] = predecessor of node i
-    v = np.zeros(n, dtype=np.int8) # v[i] is True if node is already visited False otherwise
-    h = []
+def dijkstra(G, i):
+    assert isinstance(G, dict)
+    d = {}; p = {}; v = {}
+    for j in G.keys():
+        d[j] = np.inf # d[i] = distance to node i
+        p[j] = 0      # p[i] = predecessor of node i
+        v[j] = False  # v[i] is True if node is already visited False otherwise
 
     # init
+    h = []
     heapq.heappush(h, (0, i))
     d[i] = 0
     p[i] = i
     while len(h) > 0:
         (dij, j) = heapq.heappop(h)
+        print 'j = %2d, dij = %4.2lf' % (j, dij)
         if v[j] : continue
         for k in G[j]:
             dijk = dij + G[j][k]
             if dijk < d[k]:
+                print '   k = %2d, dijk = %4.2lf' % (k, dijk)
                 d[k] = dijk
                 p[k] = j
                 heapq.heappush(h, (dijk, k))
         v[j] = True
     return d, p
 
+def dijkstra_path(i, j, p):
+    print(p)
+    s = [j]
+    while(p[j] is not i):
+        s.append(p[j])
+        j = p[j]
+    s.append(i)
+    return s
+
+
+def ita(edges, matod, cost, fracs=[.7,.1,.1,.1]):
+    print('ITA')
+    assert isinstance(edges, pd.DataFrame)
+    print('   Creating dictionaries')
+    T = {} # time
+    K = {} # capacity
+    V = {} # volumes
+    for index, row in edges.iterrows():
+        i = int(row.o)
+        j = int(row.d)
+        if i not in T.keys():
+            T[i] = {}
+            K[i] = {}
+            V[i] = {}
+        if j not in T[i]:
+            T[i][j] = row.ftt
+            K[i][j] = row.capacity
+            V[i][j] = 0
+
+    C = T.copy() # init costs (free travel time)
+
+    fracs = np.array(fracs)
+
+    for f in fracs:
+        for index, row in matod.iterrows():
+            print 'o: %d, d:%d' % (row.o, row.d)
+            vol = row.vol * f
+
+            # find shortest path
+            c, p = dijkstra(C, row.o) # cost and predecessors
+            raise Exception('NotImplemented')
+            print dijkstra_path(row.o, row.d, p)
+            # update volumes and costs
+            j = row.d
+            while p[j] is not row.o:
+                i = p[j]
+                print 'Updating edge (%2d,%2d)' % (i, j)
+                V[i][j] += vol
+                C[i][j] = cost(V[i][j], T[i][j], K[i][j])
+                j = p[j]
+
+
 def __test_dial():
-    # load dial instance
+    # load instance defined in Dial2006
     edges = pd.read_csv('../instances/dial_edges.txt', sep=' ')
-    print(edges)
+    matod = pd.read_csv('../instances/dial_od.txt', sep=' ')
+
+    # cost function from Dial2006
+    cost = lambda xij, tij, kij: tij * (1 + 0.15 * (xij/kij)**4)
+
+    ita(edges, matod, cost)
 
 def __test_dijkstra():
     G = {0: {1: 2, 3: 1},
