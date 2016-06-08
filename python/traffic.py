6a315bfa981291cb1b48b53af93c666ff8ae9692
Michael Souza <souza.michael@gmail.com>
2016-06-08 19:12:23 -0300

    Refactoring instances paths and methods visibility

--- python/traffic.py
+++ python/traffic.py
@@ -1,4 +1,3 @@
-#%% Import section
 import numpy as np
 from subprocess import check_output
 import pandas as pd
@@ -6,14 +5,12 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 from scipy.stats import lognorm
 from types import *
-#%% Utils
-        
+
+
 def numberOfLines(filename):
     # Returns the number of lines in a file without open it
-    return int(check_output(['wc','-l',filename]).split()[0])
+    return int(check_output(['wc', '-l', filename]).split()[0])
 
-
-#%% Class City
 
 class City(object):
     def __init__(self, city_name):
@@ -21,6 +18,7 @@ class City(object):
         self.nodes = self.read_nodes(city_name)
         self.edges = self.read_edges(city_name, self.nodes)
 
+        # set sizes
         self.nnodes = self.nodes['lat'].size
         self.nedges = self.edges.source.size
 
@@ -30,7 +28,7 @@ class City(object):
         t = self.edges.target
         self._D = {}
         for i in range(s.size):
-            if(not self._D.has_key(s[i])):
+            if (not self._D.has_key(s[i])):
                 self._D[s[i]] = {}
             self._D[s[i]][t[i]] = d[i]
 
@@ -38,28 +36,28 @@ class City(object):
 
     @staticmethod
     def read_nodes(city_name):
-        filename = 'instances/' + city_name + '_nodes_algbformat.txt'
+        filename = '/home/michael/mit/instances/' + city_name + '_nodes_algbformat.txt'
         print('Reading nodes from ' + filename)
         nodes = np.genfromtxt(filename, delimiter=' ', names=True)
         nnodes = int(max(nodes['nid']))
 
-        nid = (nodes['nid'] - 1).astype(np.int) # converting to zero-based
+        nid = (nodes['nid'] - 1).astype(np.int)  # converting to zero-based
         lat = np.zeros(nnodes, dtype=np.float)
         lon = np.zeros(nnodes, dtype=np.float)
 
         for i in range(nid.size):
             lat[nid[i]] = nodes['lat'][i]
             lon[nid[i]] = nodes['lon'][i]
-        return dict(lat=lat,lon=lon)
+        return dict(lat=lat, lon=lon)
 
     @staticmethod
     def read_edges(city_name, nodes):
-        filename = 'instances/' + city_name + '_edges_algbformat.txt'
+        filename = '/home/michael/mit/instances/' + city_name + '_edges_algbformat.txt'
         print('Reading edges from ' + filename)
         edges = pd.read_csv(filename, sep=' ')
-        edges = edges[edges.eid > -1]   # remove artificial edges
-        edges.source = edges.source - 1 # convert to zero-based
-        edges.target = edges.target - 1 # convert to zero-based
+        edges = edges[edges.eid > -1]  # remove artificial edges
+        edges.source = edges.source - 1  # convert to zero-based
+        edges.target = edges.target - 1  # convert to zero-based
         edges['dist_km'] = City.__calc_dist_all(nodes, edges)
         return edges
 
@@ -73,19 +71,19 @@ class City(object):
         t = edges.target
         d = np.zeros(s.size, dtype=np.float)
         for i in range(s.size):
-            d[i] = City.calc_dist(nodes, s[i], t[i])
+            d[i] = City.__calc_dist(nodes, s[i], t[i])
         return d
 
     def show(self):
         print('City: ' + self.name)
-        print('   #nodes: %d'%(self.nnodes))
-        print('   #edges: %d'%(self.nedges))
+        print('   #nodes: %d' % (self.nnodes))
+        print('   #edges: %d' % (self.nedges))
 
     def show_node(this, nid, zero_based=True):
-        print('Node[%d] %.7lf, %.7lf' %(nid, this.nodes['lat'][nid], this.nodes['lon'][nid]))
+        print('Node[%d] %.7lf, %.7lf' % (nid, this.nodes['lat'][nid], this.nodes['lon'][nid]))
 
     @staticmethod
-    def calc_dist(nodes, node_i, node_j):
+    def __calc_dist(nodes, node_i, node_j):
         # get nodes cartesian coordinates
         lat1 = nodes['lat'][node_i]
         lon1 = nodes['lon'][node_i]
@@ -94,72 +92,70 @@ class City(object):
 
         # Earth radius in kilometers
         R = 6371.009
-        factor = np.pi/180; # degrees to radians
+        factor = np.pi / 180  # degrees to radians
         mlat = factor * (lat1 + lat2) / 2
         dlat = factor * (lat1 - lat2)
         dlon = factor * (lon1 - lon2)
 
-        dij = R * np.sqrt(dlat**2+(np.cos(mlat)*dlon)**2)
+        dij = R * np.sqrt(dlat ** 2 + (np.cos(mlat) * dlon) ** 2)
         return dij
 
     def show_edge(this, eid, zero_based=True):
         s = this.edges['s'][eid]
         t = this.edges['t'][eid]
-        print('  s[%5d] %.7lf, %.7lf' %(s + (not zero_based), this.nodes['lat'][s], this.nodes['lon'][s]))
-        print('  t[%5d] %.7lf, %.7lf' %(t + (not zero_based), this.nodes['lat'][t], this.nodes['lon'][t]))
-        print('  dij: %lf km'%(this.calc_dist(s,t)))
+        print('  s[%5d] %.7lf, %.7lf' % (s + (not zero_based), this.nodes['lat'][s], this.nodes['lon'][s]))
+        print('  t[%5d] %.7lf, %.7lf' % (t + (not zero_based), this.nodes['lat'][t], this.nodes['lon'][t]))
+        print('  dij: %lf km' % (this.calc_dist(s, t)))
 
-
-#%% Class MatOD
 
 class MatOD(object):
     def __init__(this, filename, city=None):
         print('Reading MatOD from ' + filename)
-
+        assert isinstance(filename, StringType)
         this.name = filename
-        npaths = numberOfLines(filename) - 1 # number of paths
+        npaths = numberOfLines(filename) - 1  # number of paths
         this.o = np.zeros(npaths, dtype=np.int)
         this.d = np.zeros(npaths, dtype=np.int)
         this.vol = np.zeros(npaths, dtype=np.int)
         this.tt = np.zeros(npaths, dtype=np.float)
         this.path = []
-        rmod = dict(o=[],d=[],tt=[],vol=[])
+        rmod = dict(o=[], d=[], tt=[], vol=[])
         with open(filename) as fid:
             # skip header
             fid.readline()
             # read remaining rows
-            issues = dict(zero_len=dict(pth=0,vol=0), zero_vol=dict(pth=0,vol=0), too_slow=dict(pth=0,vol=0))
+            issues = dict(zero_len=dict(pth=0, vol=0), zero_vol=dict(pth=0, vol=0), too_slow=dict(pth=0, vol=0))
             npaths = 0
             totPth = 0
             totVol = 0
             for rawline in fid.xreadlines():
-                rawdata = np.fromstring(rawline, dtype=np.float,sep=' ')
-                this.o[npaths] = rawdata[0] - 1 # convert to zero-based
-                this.d[npaths] = rawdata[1] - 1 # convert to zero-based
+                rawdata = np.fromstring(rawline, dtype=np.float, sep=' ')
+                this.o[npaths] = rawdata[0] - 1  # convert to zero-based
+                this.d[npaths] = rawdata[1] - 1  # convert to zero-based
                 this.vol[npaths] = rawdata[2]
                 this.tt[npaths] = rawdata[3]
 
                 remove = False
-                totVol += 1.0  * this.vol[npaths]
+                totVol += 1.0 * this.vol[npaths]
                 totPth += 1.0
                 # skipping zero length paths and
-                if(this.o[npaths] == this.d[npaths]):
+                if (this.o[npaths] == this.d[npaths]):
                     issues['zero_len']['vol'] += this.vol[npaths]
                     issues['zero_len']['pth'] += 1
                     remove = True
 
                 # skipping paths with travel time (tt) larger than 120 min
-                if(this.tt[npaths] > 120):
+                if (this.tt[npaths] > 120):
                     issues['too_slow']['vol'] += this.vol[npaths]
                     issues['too_slow']['pth'] += 1
                     remove = True
 
                 # paths without travelers
-                if(this.vol[npaths] == 0):
+                if (this.vol[npaths] == 0):
                     issues['zero_vol']['pth'] += 1
                     remove = True
 
-                if remove :
+                if remove:
                     rmod['o'].append(this.o[npaths] + 1)
                     rmod['d'].append(this.d[npaths] + 1)
                     rmod['tt'].append(this.tt[npaths])
@@ -168,71 +164,79 @@ class MatOD(object):
 
                 # read path (if there is one to be read)
                 path = []
-                if(rawdata.size > 4):
-                    path = rawdata[4:].astype(int) - 1 # convert to int and zero-based
-                    path = np.flipud(path)             # order inversion
+                if (rawdata.size > 4):
+                    path = rawdata[4:].astype(int) - 1  # convert to int and zero-based
+                    path = np.flipud(path)  # order inversion
                 this.path.append(path)
-                npaths+=1
+                npaths += 1
 
         # saving removed od pairs
-        assert isinstance(filename, StringType)
-        filename = filename.replace('.txt','_removed.txt')
-        print '   Saving file %s' % filename
-        rmod = pd.DataFrame(rmod)
-        rmod.to_csv(filename, sep=' ', index=False)
+        if len(rmod['o']) > 0:
+            filename = filename.replace('.txt', '_removed.txt')
+            print '   Saving file %s' % filename
+            rmod = pd.DataFrame(rmod)
+            rmod.to_csv(filename, sep=' ', index=False)
 
         # reshaping paths
         this.npaths = npaths
-        if npaths < this.o.size :
+        if npaths < this.o.size:
             dsize = this.o.size - npaths
             print '   Reshaping by eliminating %d paths' % dsize
-            print '      zero_volume       = %5d (#path: %2.1f%% vol: %2.1f%%)  ' % (issues['zero_vol']['pth'], 100 * issues['zero_vol']['pth']/totPth, (100 * issues['zero_vol']['vol'])/totVol)
-            print '      zero_length       = %5d (#path: %2.1f%% vol: %2.1f%%)' % (issues['zero_len']['pth'], 100 * issues['zero_len']['pth']/totPth, (100 * issues['zero_len']['vol'])/totVol)
-            print '      too_slow(>120min) = %5d (#path: %2.1f%% vol: %2.1f%%)' % (issues['too_slow']['pth'], 100 * issues['too_slow']['pth']/totPth, (100 * issues['too_slow']['vol'])/totVol)
+            print '      zero_volume       = %5d (#path: %2.1f%% vol: %2.1f%%)  ' % (
+            issues['zero_vol']['pth'], 100 * issues['zero_vol']['pth'] / totPth,
+            (100 * issues['zero_vol']['vol']) / totVol)
+            print '      zero_length       = %5d (#path: %2.1f%% vol: %2.1f%%)' % (
+            issues['zero_len']['pth'], 100 * issues['zero_len']['pth'] / totPth,
+            (100 * issues['zero_len']['vol']) / totVol)
+            print '      too_slow(>120min) = %5d (#path: %2.1f%% vol: %2.1f%%)' % (
+            issues['too_slow']['pth'], 100 * issues['too_slow']['pth'] / totPth,
+            (100 * issues['too_slow']['vol']) / totVol)
             this.o = this.o[:npaths]
             this.d = this.d[:npaths]
             this.vol = this.vol[:npaths]
             this.tt = this.tt[:npaths]
             this.path = this.path[:npaths]
-        
+
         # calc path length in km
         this.city = city
-        if(city is not None):
+        if (city is not None):
             this.dij = np.zeros(this.npaths, dtype=np.float)
             for i in range(this.npaths):
                 this.dij[i] = this.path_length(this.path[i], city)
-        
+
         # calc path velocity in km/h
         this.vij = 60 * this.dij / this.tt
-        
+
         this.show()
 
-    def __check(this):        
+    def __check(this):
         for i in range(this.npaths):
-            if(this.tt[i] == 0):
-                print('Edge #%d (%d,%d) zeroed travel time'%(i, this.o[i], this.d[i]))
+            if (this.tt[i] == 0):
+                print('Edge #%d (%d,%d) zeroed travel time' % (i, this.o[i], this.d[i]))
 
     @staticmethod
     def path_length(path, city):
         dij = 0
-        for j in range(path.size-1):
-            dij += city.dist(path[j], path[j+1])
+        for j in range(path.size - 1):
+            dij += city.dist(path[j], path[j + 1])
         return dij
 
     def show_path(this, pid, zero_based=False, verbose=False):
-        if(this.city is not None):
+        if (this.city is not None):
             s = this.o[pid]
             t = this.d[pid]
-            print('Path[%d]'%(pid))
+            print('Path[%d]' % (pid))
             for i in this.path[pid]:
-                print('Node %5d: %.8lf, %.8lf' %(i, this.city.nodes['lat'][i], this.city.nodes['lon'][i]))
-            print('  o[%5d] %.7lf, %.7lf' %(s + (not zero_based), this.city.nodes['lat'][s], this.city.nodes['lon'][s]))
-            print('  d[%5d] %.7lf, %.7lf' %(t + (not zero_based), this.city.nodes['lat'][t], this.city.nodes['lon'][t]))
-            print('  dij: %lf km'%(this.dij[pid]))
+                print('Node %5d: %.8lf, %.8lf' % (i, this.city.nodes['lat'][i], this.city.nodes['lon'][i]))
+            print(
+            '  o[%5d] %.7lf, %.7lf' % (s + (not zero_based), this.city.nodes['lat'][s], this.city.nodes['lon'][s]))
+            print(
+            '  d[%5d] %.7lf, %.7lf' % (t + (not zero_based), this.city.nodes['lat'][t], this.city.nodes['lon'][t]))
+            print('  dij: %lf km' % (this.dij[pid]))
 
     def show(this):
-        print('MatOD: %s'%this.name)
-        print('  #paths: %d'%this.npaths)
+        print('MatOD: %s' % this.name)
+        print('  #paths: %d' % this.npaths)
 
     def write_wkt_paths(this, pids, filename):
         with open(filename, 'w') as fid:
@@ -240,7 +244,7 @@ class MatOD(object):
                 print('Writing wkt path ')
                 this.show_path(pid)
                 path = this.path[pid]
-                fid.write('%d;LINESTRING('%(pid))
+                fid.write('%d;LINESTRING(' % (pid))
                 for j in range(len(path)):
                     nid = path[j]
                     fid.write('%.8lf %.8lf' % (this.city.nodes['lon'][nid], this.city.nodes['lat'][nid]))
@@ -248,69 +252,66 @@ class MatOD(object):
                         fid.write(')\n')
                     else:
                         fid.write(',')
-                        
+
     def get_table(this):
-        return pd.DataFrame({'dij':this.dij,'vij':this.vij,'tt':this.tt,'vol':this.vol})
+        return pd.DataFrame({'dij': this.dij, 'vij': this.vij, 'tt': this.tt, 'vol': this.vol})
 
 
-#%% Analysis
-def hist(x,weights=None,bins=10,distname='normal',color='b',label='pdf',filename=None):
+def hist(x, weights=None, bins=10, distname='normal', color='b', label='pdf', filename=None):
     # create full data using weights
     z = x
-    if(weights is not None):
+    if (weights is not None):
         z = np.zeros(sum(weights))
         j = 0
-        for i in range(weights.size):        
-            for k in range(j,j+weights[i]):
+        for i in range(weights.size):
+            for k in range(j, j + weights[i]):
                 z[j] = x[i]
                 j += 1
-    
+
     # histogram
-    hist, bins = np.histogram(x, bins=bins, density=True, weights=weights)    
-    
+    hist, bins = np.histogram(x, bins=bins, density=True, weights=weights)
+
     # fit distribution
-    if(distname is 'normal'):
+    if (distname is 'normal'):
         (mu, sigma) = norm.fit(z)
-        pdf = lambda x: norm.pdf(x,mu,sigma)
-    elif(distname is 'lognormal'):
-        sigma, loc, scale = lognorm.fit(z,floc=0)
+        pdf = lambda x: norm.pdf(x, mu, sigma)
+    elif (distname is 'lognormal'):
+        sigma, loc, scale = lognorm.fit(z, floc=0)
         mu = np.log(scale)
-        pdf = lambda x: lognorm.pdf(x,sigma,loc,scale=scale)
-    elif(distname is not None):
+        pdf = lambda x: lognorm.pdf(x, sigma, loc, scale=scale)
+    elif (distname is not None):
         raise Exception('Unsupported distribution name ' + distname)
-                
+
     # plot distribution
-    if(distname is not None):
+    if (distname is not None):
         x = np.linspace(bins[0], bins[-1], 100)
         y = pdf(x)
         label = 'm=%2.1f, s=%2.1f [%s]' % (mu, sigma, label)
         plt.plot(x, y, linewidth=3, label=label, alpha=0.7, color=color)
-    
+
     # plot histogram
-    c = (bins[:-1] + bins[1:]) / 2; # bins centers
-    plt.plot(c, hist,marker='s',alpha=0.7,markersize=8,linestyle='None',color=color)
+    c = (bins[:-1] + bins[1:]) / 2;  # bins centers
+    plt.plot(c, hist, marker='s', alpha=0.7, markersize=8, linestyle='None', color=color)
 
     # format plot    
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
-    plt.ylabel('PDF',fontsize=16)
-    
-    if(filename is not None):
+    plt.ylabel('PDF', fontsize=16)
+
+    if (filename is not None):
         print('Saving figure ' + filename)
         plt.savefig(filename, bbos_inches='tight')
 
 
-def comparing_changes(tabA, tabB):
+def analyse_tt(tabA, tabB):
     raise Exception('NotImplementedMethod')
 
-#%% Load data
-if __name__== '__main__':
-     # load city
-     city = City('sfbay')
 
-     hist(city.edges.capacity, distname=None)
-#
-#     # load matod
+if __name__ == '__main__':
+    # load city
+    city = City('sfbay')
+
+# # load matod
 #     matod = {'10':[], 'voc_05':[], 'btwall_05':[], 'clus_05':[]}
 #     for key in matod.keys():
 #         matod[key] = MatOD('instances/Results_ita/' + city.name + '_' + key + '_od_a.txt', city)
