b92d7f3893ce8b359b315bc52e04bbe18e58eb01
Michael Souza <souza.michael@gmail.com>
2016-06-13 11:53:40 -0300

    Refactoring dijkstra_multipath

--- python/traffic_assignment.py
+++ python/traffic_assignment.py
@@ -17,84 +17,82 @@ def load_matod(filename):
     return matod
 
 
-def dijkstra_multipath(G, i, verbose=False):
+def dijkstra_multipath(G, i, dtol=0.01, verbose=False):
+    """
+
+    :type dtol: float
+    :type verbose: bool
+    """
     assert isinstance(G, dict)
-    dtol = 0.01
-    d = {}
-    p = {}
-    v = {}
+    di = {}
+    pred = {}
+    visited = {}
     for j in G.keys():
-        d[j] = np.inf  # d[i] = distance to node i
-        p[j] = 0  # p[i] = predecessor of node i
-        v[j] = False  # v[i] is True if node is already visited False otherwise
+        di[j] = np.inf  # d[i] = distance to node i
+        pred[j] = {}      # p[i] = predecessors of node i
+        visited[j] = False   # v[i] is True if node is already visited False otherwise
 
     # init
     h = []
     heapq.heappush(h, (0, i))
-    d[i] = 0
-    p[i] = i
-    while len(h) > 0:
-        J = []
-        D = []
-        # find all equivalent paths
-        while len(h) > 0:
-            (dij, j) = heapq.heappop(h)
-            # add alternative node
-            if (len(J) == 0) or ((dij - D[0])/D[0] < dtol): 
-                J.append(j)
-                D.append(dij)
-                continue
-            # reinsert node j - it's not an equivalent alternative
-            heappush(h, (dij, j))
-            break
-
-        if verbose: 
-            for k in range(len(J)):
-                print 'j = %2d, dij = %4.2lf' % (J[k], D[k])
-        
-        for q in range(len(J)):
-            j = J[q]
-            # already visited
-            if v[j]: continue
-            dij = D[q]
-            for k in G[j]:
-                dijk = dij + G[j][k]
-                if dijk < d[k]:
-                    if verbose: print '   k = %2d, dijk = %4.2lf' % (k, dijk)
-                    d[k] = dijk
-                    p[k] = j
-                    heapq.heappush(h, (dijk, k))
-            v[j] = True
-    return d, p
-
-def dijkstra(G, i, verbose=False):
-    assert isinstance(G, dict)
-    d = {}
-    p = {}
-    v = {}
-    for j in G.keys():
-        d[j] = np.inf  # d[i] = distance to node i
-        p[j] = 0  # p[i] = predecessor of node i
-        v[j] = False  # v[i] is True if node is already visited False otherwise
-
-    # init
-    h = []
-    heapq.heappush(h, (0, i))
-    d[i] = 0
-    p[i] = i
+    di[i] = 0.0
+    pred[i][i] = (i, 0)
     while len(h) > 0:
         (dij, j) = heapq.heappop(h)
         if verbose: print 'j = %2d, dij = %4.2lf' % (j, dij)
-        if v[j]: continue
+        if visited[j]: continue
         for k in G[j]:
             dijk = dij + G[j][k]
-            if dijk < d[k]:
-                if verbose: print '   k = %2d, dijk = %4.2lf' % (k, dijk)
-                d[k] = dijk
-                p[k] = j
+            # update shortest path
+            if dijk < di[k]:
+                if verbose: print '   update k = %2d, dijk = %4.2lf' % (k, dijk)
+                di[k] = float(dijk)
+                # remove not equivalent path
+                for z in pred[k].keys():
+                    (z, dizk) = pred[k][z]
+                    if abs(dizk - dijk) / dijk > dtol:
+                        if verbose: print '      remove (%2d, %2d) = %4.2lf' %(z, k, dizk)
+                        pred[k].pop(z)
+                pred[k][j] = (j, dijk)
                 heapq.heappush(h, (dijk, k))
-        v[j] = True
-    return d, p
+            # add equivalent path
+            elif abs(dijk - di[k]) / max(di[k], 1.0) < dtol:
+                if verbose: print '   add   k = %2d, dik = %4.2lf, dijk = %4.2lf' % (k, di[k], dijk)
+                pred[k][j] = (j, dijk)
+                heapq.heappush(h, (dijk, k))
+
+        visited[j] = True
+    return di, pred
+
+
+def dijkstra(G, i, verbose=False):
+    assert isinstance(G, dict)
+    di = {}
+    pred = {}
+    visited = {}
+    for j in G.keys():
+        di[j] = np.inf  # d[i] = distance to node i
+        pred[j] = 0  # p[i] = predecessor of node i
+        visited[j] = False  # v[i] is True if node is already visited False otherwise
+
+    # init
+    h = []
+    heapq.heappush(h, (0, i))
+    di[i] = 0
+    pred[i] = i
+    while len(h) > 0:
+        (dij, j) = heapq.heappop(h)
+        if verbose: print 'j = %2d, dij = %4.2lf' % (j, dij)
+        if visited[j]: continue
+        for k in G[j]:
+            dijk = dij + G[j][k]
+            if dijk < di[k]:
+                if verbose: print '   k = %2d, dijk = %4.2lf' % (k, dijk)
+                di[k] = dijk
+                pred[k] = j
+                heapq.heappush(h, (dijk, k))
+        visited[j] = True
+    return di, pred
 
 
 def print_dijkstra_path(i, j, p):
@@ -109,6 +107,75 @@ def print_dijkstra_path(i, j, p):
     print(s)
     return s
 
+def ita_multipath(edges, matod, cost, fracs=None, verbose=False):
+    if fracs is None:
+        fracs = np.ones(100)/100
+
+    if sum(fracs) < 0.99:
+        raise Exception('The input sum(fracs) must be equal to one.')
+
+    assert isinstance(edges, pd.DataFrame)
+
+    # read nodes from edges
+
+    if verbose: print 'ITA'
+    if verbose: print '   Creating data structures'
+    T = {}  # time
+    K = {}  # capacity
+    V = {}  # volumes
+    C = {}  # cost
+    for index, row in edges.iterrows():
+        i = int(row.o)
+        j = int(row.d)
+        if i not in T.keys():
+            T[i] = {}
+            K[i] = {}
+            V[i] = {}
+            C[i] = {}
+        if j not in T.keys():
+            T[j] = {}
+            K[j] = {}
+            V[j] = {}
+            C[j] = {}
+        if j not in T[i]:
+            T[i][j] = row.ftt
+            K[i][j] = row.capacity
+            V[i][j] = 0
+            C[i][j] = 0
+
+    assert isinstance(matod, dict)
+    if verbose: print '   Finding shortest paths'
+    iter = 0
+    for f in fracs:
+        for i in matod.keys():
+            if verbose: print 'Find paths from node %d' % i
+            # find shortest path from i to all nodes
+            c, p = dijkstra(C, i) # cost and predecessors
+
+            # update volumes
+            for j in matod[i].keys():
+                if verbose: print_dijkstra_path(i, j, p)
+                ej = j # edge end node
+                while True:
+                    ei = p[ej] # edge begin node
+                    # update volume
+                    V[ei][ej] += matod[i][j] * f
+                    # path origin has been found
+                    if ei == i: break
+                    ej = ei
+
+        # update costs
+        for i in V.keys():
+            for j in V[i].keys():
+                C[i][j] = cost(V[i][j], T[i][j], K[i][j])
+
+        # view V and C
+        if verbose: print_dict(V, 'Vols iter %d' % iter)
+        if verbose: print_dict(C, 'Cost iter %d' % iter)
+        iter += 1
+
+    quality = traffic_assignment_quality(matod, V, C)
+    return V, C, quality
 
 def ita(edges, matod, cost, fracs=None, verbose=False):
     if fracs is None:
@@ -119,6 +186,7 @@ def ita(edges, matod, cost, fracs=None, verbose=False):
 
     assert isinstance(edges, pd.DataFrame)
 
+    # todo Remove unused variable nodes
     # read nodes from edges
     nodes = []
 
@@ -227,9 +295,9 @@ def print_dict(V, label):
 
 def __test_small__():
     # load instance
-    edges = pd.read_csv('../instances/dial_edges.txt', sep=' ')
+    edges = pd.read_csv('../instances/smallA_edges.txt', sep=' ')
     # load matod
-    matod = load_matod('../instances/dial_od.txt')
+    matod = load_matod('../instances/smallA_od.txt')
     # cost function
     cost = lambda xij, tij, kij: tij + (xij / kij)
 
@@ -260,7 +328,24 @@ def __test_dijkstra__():
     d, p = dijkstra(G, 0)
     print d, p
 
+def __test_dijkstra_multipath__():
+    # load instance
+    edges = pd.read_csv('../instances/smallA_edges.txt', sep=' ')
+
+    # convert edges to graph
+    G = {}
+    for index, row in edges.iterrows():
+        if row.o not in G.keys():
+            G[row.o] = {}
+        G[row.o][row.d] = row.ftt
+    print G
+
+    d, p = dijkstra_multipath(G, 1, verbose=True)
+    print 'd = ', d
+    print 'p = ', p
+
 
 if __name__ == '__main__':
     # __test_dial__()
-    __test_small__()
+    # __test_small__()
+    __test_dijkstra_multipath__()
