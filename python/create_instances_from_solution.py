664ab8f92be2c0089f65556031b4dc298f7d11bf
michael <souza.michael@gmail.com>
2017-01-24 11:56:40 -0200

    Improving column description

--- python/create_instances_from_solution.py
+++ python/create_instances_from_solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-problem = 'porto'
+problem = 'boston'
 MAX_DIST = {50, 100, 250, 500}
 ALPHA = {0.1, 0.2, 0.5, 0.7, 1.0}
 RANK = {'btw_id', 'voc_id'}
@@ -14,48 +14,54 @@ for alpha in ALPHA:
             # load table of ranks
             print('Reading table of ranks: %s' % fid_tab)
             table = pd.read_csv(fid_tab)
-
-            table_gid = table['eid'].as_matrix()
-            table_dij = table['dij_km'].as_matrix()
-            table_rank_id = table[rank].as_matrix()
-
-            rank_eid = dict()
-            for eid in range(len(table_rank_id)):
-                rank_eid[int(table_rank_id[eid])] = eid
-
+            
+            print('Converting table to arrays')
+            eids   = table['eid'].as_matrix()
+            source = table['source'].as_matrix()
+            target = table['target'].as_matrix()
+            dist   = table['length_km'].as_matrix()
+            idrank = table[rank].as_matrix()
+            nedges = len(source)
+            
+            # (rank_id) -> row
+            map_rank_to_row = dict()
+            for k in range(nedges):
+                map_rank_to_row[idrank[k]] = k
+                
             print('Selecting edges')
-            selected_eids = []
+            selected_rows = []
             total_dist = 0
-            for k in range(len(rank_eid)):
-                eid = rank_eid[k]
-                gid = table_gid[eid]
-                selected_eids.append(eid)
+            for k in range(min(idrank), max(idrank) + 1):
+                row = map_rank_to_row[k]
+                selected_rows.append(row)
                 
                 # consider only the length of non-artificial edges
-                if gid >= 0:
-                    total_dist += table_dij[eid]
+                eid = eids[row]
+                if eid >= 0:
+                    total_dist += dist[row]
                 else:
-                    print('The length of edge %d was not added' % gid)
+                    print('The length of edge (%d,%d) was not added' % (source[row], target[row]))
                 
                 # stop criteria
                 if total_dist >= max_dist:
                     break
-            print('   Number of selected edges: %d' % len(selected_eids))
+            print('   Number of selected edges: %d' % len(selected_rows))
             print('   Total distance (km):      %f' % total_dist)
 
             # create selected column
-            selected = np.zeros(len(rank_eid), dtype=int)
-            for eid in selected_eids:
-                selected[eid] = 1
+            selected = np.zeros(nedges, dtype=int)
+            for row in selected_rows:
+                selected[row] = 1
             table['selected'] = pd.Series(selected, index=table.index)
 
             # incrementing the edge capacities
-            cap = table['cap'].as_matrix()
+            cap = table['capacity'].as_matrix()
             cap_old = cap.copy()
-            for eid in selected_eids:
-                cap[eid] *= (1 + alpha)
-            table['cap'] = pd.Series(cap, index=table.index)
-            table['cap_old'] = pd.Series(cap_old, index=table.index)
+            for row in selected_rows:
+                cap[row] *= (1 + alpha)
+            table['capacity'] = pd.Series(cap, index=table.index)
+            table['capacity_old'] = pd.Series(cap_old, index=table.index)
 
-            print('Saving instance')
-            table.to_csv('../instances/%s_R%s_A%3.2f_D%03d.csv' % (problem, rank, alpha, max_dist), index=False)
+            instance_name = '%s_R%s_A%3.2f_D%03d.csv' % (problem, rank, alpha, max_dist)
+            print('Saving instance: %s' % instance_name)
+            table.to_csv(instance_name, index=False)
